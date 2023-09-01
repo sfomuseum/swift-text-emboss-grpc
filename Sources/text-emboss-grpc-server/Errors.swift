755f189a234b729c1cfb6493f523e0b62aa4b151
sfomuseumbot <sfomuseumbot@localhost>
2023-09-01 11:25:41 -0700

    snapshot: huh, it works

---
+++ Sources/text-emboss-grpc-server/Errors.swift
@@ -0,0 +1,8 @@
+public enum Errors: Error {
+    case notFound
+    case invalidImage
+    case cgImage
+    case processError
+    case unsupportedOS
+    case fileManager
+}
--- Sources/text-emboss-grpc-server/provider.swift
+++ Sources/text-emboss-grpc-server/provider.swift
@@ -1,66 +1,57 @@
 import GRPC
 import NIOCore
+import Foundation
+import AppKit
+import TextEmboss
 
-@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
+@available(macOS 10.15, *)
 final class TextEmbossProvider: EmbosserAsyncProvider {
   let interceptors: EmbosserServerInterceptorFactoryProtocol? = nil
-        
+    
     func embossText(request: EmbossTextRequest, context: GRPC.GRPCAsyncServerCallContext) async throws -> EmbossTextResponse {
     
-        /*
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(),
+                                            isDirectory: true)
+        
+        let temporaryFilename = ProcessInfo().globallyUniqueString
+
+        let temporaryFileURL =
+            temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
+        
+        try request.body.write(to: temporaryFileURL,
+                       options: .atomic)
+        
+        defer {
+            do {
+                try FileManager.default.removeItem(at: temporaryFileURL)
+            } catch {
+                print(error)
+            }
+        }
          
-         let uuid = UUID().uuidString
-         let fname = uuid + ext
+        guard let im = NSImage(byReferencingFile:temporaryFileURL.path) else {
+            throw(Errors.invalidImage)
+        }
          
-         let data: NSData = myFileMultipart.body.withUnsafeBufferPointer { pointer in
-             return NSData(bytes: pointer.baseAddress, length: myFileMultipart.body.count)
-         }
+        guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            throw(Errors.cgImage)
+        }
          
-         guard let fileSaveUrl = NSURL(string: fname, relativeTo: documentsUrl) else {
-             logger.error("Failed to derive file save URL")
-             return .internalServerError
-         }
-         
-         guard data.write(to: fileSaveUrl as URL, atomically: true) else {
-             logger.error("Failed to write image data")
-             return .internalServerError
-         }
-         
-         defer {
-             do {
-                 try FileManager.default.removeItem(at: fileSaveUrl as URL)
-             } catch {
-                 logger.error("Failed to remove \(fileSaveUrl.path!), \(error)")
-             }
-         }
-         
-         guard let im = NSImage(byReferencingFile:fileSaveUrl.path!) else {
-             logger.error("Invalid image")
-             return .badRequest(.text("Invalid image"))
-         }
-         
-         guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-             logger.error("Failed to derive CG image")
-             return .internalServerError
-         }
-         
-         let te = TextEmboss()
-         let rsp = te.ProcessImage(image: cgImage)
+        let te = TextEmboss()
+        let rsp = te.ProcessImage(image: cgImage)
          
          switch rsp {
          case .failure(let error):
-             logger.error("Failed to process image, \(error)")
-             return .internalServerError
+             throw(error)
          case .success(let txt):
-             return .ok(.text(txt))
+             
+             return EmbossTextResponse.with{
+                 $0.filename = request.filename
+                 $0.body = txt
+             }
+             
          }
          
-         */
-        
-        return EmbossTextResponse.with{
-            $0.filename = request.filename
-            $0.body = ""
-        }
     }
     
 
