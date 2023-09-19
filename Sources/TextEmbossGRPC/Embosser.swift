55dfb0e9fb2dae33ebee3a1686c39562a9f8fcdf
sfomuseumbot <sfomuseumbot@localhost>
2023-09-19 12:54:55 -0700

    make EmbossText class public

--- Sources/TextEmbossGRPC/Embosser.swift
+++ Sources/TextEmbossGRPC/Embosser.swift
@@ -5,7 +5,7 @@ import AppKit
 import TextEmboss
 
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, *)
-final class TextEmbosser: EmbosserAsyncProvider {
+public final class TextEmbosser: EmbosserAsyncProvider {
   let interceptors: EmbosserServerInterceptorFactoryProtocol? = nil
 
     func embossText(request: EmbossTextRequest, context: GRPC.GRPCAsyncServerCallContext) async throws -> EmbossTextResponse {
